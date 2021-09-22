7745ed7bd315617992ff316ab1d9828feb7e6d64
Mirko Lenz <info@mirko-lenz.de>
2021-09-22 11:14:21 +0200

    Re-export modules

--- arguebuf/__init__.py
+++ arguebuf/__init__.py
@@ -3,10 +3,19 @@ import logging
 
 from arg_services.graph.v1 import graph_pb2
 
-from arguebuf.data import Metadata, Participant, Reference, Resource
-from arguebuf.edge import Edge
-from arguebuf.graph import Graph, GraphFormat, render
-from arguebuf.node import AtomNode, Node, Scheme, SchemeNode, SchemeType
-from arguebuf.utils import unique_id
+from arguebuf.data import Metadata as Metadata
+from arguebuf.data import Participant as Participant
+from arguebuf.data import Reference as Reference
+from arguebuf.data import Resource as Resource
+from arguebuf.edge import Edge as Edge
+from arguebuf.graph import Graph as Graph
+from arguebuf.graph import GraphFormat as GraphFormat
+from arguebuf.graph import render as render
+from arguebuf.node import AtomNode as AtomNode
+from arguebuf.node import Node as Node
+from arguebuf.node import Scheme as Scheme
+from arguebuf.node import SchemeNode as SchemeNode
+from arguebuf.node import SchemeType as SchemeType
+from arguebuf.utils import unique_id as unique_id
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
