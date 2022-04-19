a466962d47158f2f1b30f09ee0ba43ee7ee59058
Mirko Lenz <info@mirko-lenz.de>
2022-04-19 23:16:51 +0200

    Rename sphinx project

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
-project = "ReCAP Argument Graph"
+project = "Arguebuf"
 copyright = "Mirko Lenz"
 author = "Mirko Lenz"
 
