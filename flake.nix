148d1b71c8224a1aabb02afc2db622746a410cc7
Mirko Lenz <mirko@mirkolenz.com>
2023-06-04 08:07:02 +0200

    build: update nix shell inputs

--- flake.nix
+++ flake.nix
@@ -50,9 +50,9 @@
         devShells.default = pkgs.mkShell {
           packages = [poetry python];
           propagatedBuildInputs = with pkgs; [graphviz d2];
-          buildInputs = with pkgs; [gcc glibc];
+          buildInputs = with pkgs; [gcc];
           POETRY_VIRTUALENVS_IN_PROJECT = true;
-          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.stdenv.cc.cc];
+          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.gcc];
           shellHook = ''
             ${lib.getExe poetry} env use ${lib.getExe python}
             ${lib.getExe poetry} install --all-extras --no-root
